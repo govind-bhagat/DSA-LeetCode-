b761822d206bee6699f67052512aab7251a67a89
Govind Bhagat <142727380+govind-bhagat@users.noreply.github.com>
2026-03-31 08:56:20 +0530

    Create 832. Flipping an Image.cpp

---
+++ 832. Flipping an Image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int row = image.size();
+        int column = image[0].size();
+        for(int i = 0; i<row; i++){
+            reverse(image[i].begin(), image[i].end());
+        }
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<column; j++){
+                if(image[i][j] == 0) image[i][j] = 1;
+                else image[i][j] = 0;
+            }
+        }
+        return image;
+
+    }
+};
